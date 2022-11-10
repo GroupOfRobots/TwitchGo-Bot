35bb19d393915a2756603721e902c22d3152ecc3
Mikołaj Chomanski <mikolaj.chomanski@gmail.com>
2022-11-10 18:37:00 +0100

    added position atribute

--- Bonus.py
+++ Bonus.py
@@ -1,16 +1,23 @@
 class Bonus:
-    def __init__(self, bonus_points: int, tex_file="") -> None:
+    def __init__(self, bonus_points: int, position: tuple, tex_file="") -> None:
         self._bonus_points = bonus_points
         self._texture_path = tex_file
+        self._position = position
 
-    def bonus_points(self):
+    def bonus_points(self) -> int:
         return self._bonus_points
 
-    def set_bonus_points(self, new_bonus_points: int):
+    def set_bonus_points(self, new_bonus_points: int) -> None:
         self._bonus_points = new_bonus_points
 
-    def texture_file(self):
+    def texture_file(self) -> str:
         return self._texture_path
 
-    def set_texture_file(self, new_tex_file: str):
+    def set_texture_file(self, new_tex_file: str) -> None:
         self._texture_path = new_tex_file
+
+    def position(self) -> tuple:
+        return self._position
+
+    def set_position(self, new_position: tuple) -> None:
+        self._position = new_position
