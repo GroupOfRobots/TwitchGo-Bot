7da710e904e67ee10fd3273244e4e6f6a87c7dff
Mikołaj Chomanski <mikolaj.chomanski@gmail.com>
2022-11-19 15:55:18 +0100

    canged how bonuses are displayed in the list

--- MainWindow.py
+++ MainWindow.py
@@ -1,12 +1,14 @@
 from resource_gathering_ui import Ui_MainWindow
-from PySide2.QtWidgets import QMainWindow, QApplication
+from PySide2.QtWidgets import QMainWindow, QApplication, QListWidgetItem
 import sys
+from Bonus import Bonus
 
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None) -> None:
         super().__init__(parent=parent)
         self._score = (0, 0)
+        self._bonuses = [Bonus('first bonus', 2, (1, 2))]
         self._ui = Ui_MainWindow()
         self._ui.setupUi(self)
         self._ui.score.setText(f'{self._score[0]} - {self._score[1]}')
@@ -14,6 +16,13 @@ class MainWindow(QMainWindow):
         self._ui.add_goal_second.clicked.connect(self._add_goal_second)
         self._ui.subtract_goal_first.clicked.connect(self._sub_goal_first)
         self._ui.subtract_goal_second.clicked.connect(self._sub_goal_second)
+        self._set_up_bonusses_list()
+
+    def _set_up_bonusses_list(self):
+        for bonus in self._bonuses:
+            item = QListWidgetItem(str(bonus))
+            item.bonus = bonus
+            self._ui.resources_list.addItem(item)
 
     def _add_goal_first(self):
         self._score = (self._score[0]+1, self._score[1])
@@ -39,8 +48,6 @@ def guiMain(args):
     window = MainWindow()
     window.show()
     return app.exec_()
-
-
 
 
 if(__name__ == "__main__"):
