1897d22c7eee58c465e0076589c59ff156eeb2e0
Mikołaj Chomanski <mikolaj.chomanski@gmail.com>
2022-12-04 15:48:17 +0100

    added time bonus class

---
+++ Time_Bonus.py
@@ -0,0 +1,13 @@
+from Bonus import Bonus
+
+
+class Time_Bonus (Bonus):
+    def __init__(self, name: str, multiplier: int, position: tuple, bonus_time, tex_file="") -> None:
+        super().__init__(name, multiplier, position, tex_file)
+        self._bonus_time = bonus_time
+
+    def bonus_time(self):
+        return self._bonus_time
+
+    def set_bonus_time(self, new_time):
+        self._bonus_time = new_time
