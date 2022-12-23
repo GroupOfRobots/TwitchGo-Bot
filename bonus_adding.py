6785703e54cc13d4d0b9fc86d7d4e08fb23f19b7
Mikołaj Chomanski <mikolaj.chomanski@gmail.com>
2022-12-23 18:32:32 +0100

    added switching betwean regular nad time bonus

--- bonus_adding.py
+++ bonus_adding.py
@@ -7,8 +7,10 @@ class BonusAdding(QDialog):
         super().__init__(parent)
         self._ui = Ui_Dialog()
         self._ui.setupUi(self)
+        self._set_up_bonus_choice()
 
     def show(self) -> None:
+        self._ui.ponus_creation.setCurrentIndex(0)
         self._ui.bonus_duration.setValue(0)
         self._ui.bonus_value.setValue(0)
         self._ui.position_x.setValue(0)
@@ -16,3 +18,13 @@ class BonusAdding(QDialog):
         self._ui.file_name.setText("Enter filename")
         self._ui.file_name.setText("Enter name")
         return super().show()
+
+    def _set_up_bonus_choice(self):
+        self._ui.create_regular.clicked.connect(self._change_to_regular)
+        self._ui.create_time.clicked.connect(self._change_to_time)
+
+    def _change_to_time(self):
+        self._ui.ponus_creation.setCurrentIndex(1)
+
+    def _change_to_regular(self):
+        self._ui.ponus_creation.setCurrentIndex(0)
