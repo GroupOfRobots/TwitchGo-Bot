ee32fbc1a35258dabc2450df84842d7ad97426f0
aleksanderpanasiuk <olekp249@gmail.com>
2023-05-11 15:32:11 +0200

    Added votes to traps

--- trap.py
+++ trap.py
@@ -2,10 +2,21 @@ class Trap:
     def __init__(self, name, command=None):
         self._name = name
         self._command = command
+        self._votes = []
 
     @property
     def get_name(self):
         return self._name
+
+    @property
+    def get_votes(self):
+        return self._votes
+
+    def set_votes(self, votes: list):
+        self._votes = votes
+
+    def add_vote(self, username):
+        self._votes.append(username)
 
     def run(self):
         if self._command is not None:
